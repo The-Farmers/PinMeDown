8bebee7e110676c106332622226936c5adcd3c21
Jeremy Tan <tan_kai_qun97@yahoo.com.sg>
2021-07-24 05:45:02 +0800

    Add google maps

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,27 +1,77 @@
-import GoogleMapReact from "google-map-react";
-import { useState } from "react";
-import useGeolocation from "react-hook-geolocation";
+import { useState, useCallback } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  MarkerClusterer,
+  Marker,
+} from "@react-google-maps/api";
+import useGeolocation from "../../custom-hooks/use-geolocation";
 import PlaceholderWrapper from "../placeholder-wrapper";
+import styles from "./map.module.scss";
+
+const DEFAULT_CENTER: google.maps.LatLngLiteral = {
+  lat: 1.2949,
+  lng: 103.7737,
+};
+const DEFAULT_ZOOM = 18;
+
+const MARKER_CLUSTERER_OPTIONS = {
+  imagePath: "assets/map/m",
+};
+
+const locations: google.maps.LatLngLiteral[] = [
+  { lat: 1.29, lng: 103.7717 },
+  { lat: 1.2958, lng: 103.7747 },
+  { lat: 1.2948, lng: 103.7736 },
+  { lat: 1.2927, lng: 103.7758 },
+];
+
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function Map() {
-  const [loaded, setLoaded] = useState(false);
-  const { latitude: lat, longitude: lng } = useGeolocation({}, () =>
-    setLoaded(true),
-  );
+  const { geolocation } = useGeolocation();
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
+  });
+  const [map, setMap] = useState<google.maps.Map>();
+
+  const onUnmount = useCallback(() => setMap(undefined), []);
 
   return (
     <PlaceholderWrapper
       placeholder
-      loading={!loaded}
+      loading={!loadError && (geolocation === undefined || !isLoaded)}
       loadingMessage="Loading map"
-      showDefaultMessage={lat === null || lng === null}
-      defaultMessage="Location service not enabled"
+      showDefaultMessage={Boolean(loadError)}
+      defaultMessage={
+        loadError?.message ?? "An error has occurred while loading the map"
+      }
     >
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={{ lat, lng }}
-        defaultZoom={11}
-      />
+      <GoogleMap
+        mapContainerClassName={styles.map}
+        onLoad={setMap}
+        onUnmount={onUnmount}
+        zoom={DEFAULT_ZOOM}
+        center={
+          geolocation
+            ? { lat: geolocation.latitude, lng: geolocation.longitude }
+            : DEFAULT_CENTER
+        }
+      >
+        <MarkerClusterer options={MARKER_CLUSTERER_OPTIONS}>
+          {(clusterer) =>
+            locations.map((location, index) => (
+              <Marker
+                key={index}
+                position={location}
+                label={labels[index % labels.length]}
+                clusterer={clusterer}
+              />
+            ))
+          }
+        </MarkerClusterer>
+      </GoogleMap>
     </PlaceholderWrapper>
   );
 }
