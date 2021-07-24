56854221b4a95cbdd7ba8b7db3b421e47cc0e738
maxxng <maxngkaishen@gmail.com>
2021-07-24 08:58:07 +0800

    Finish auth feature

--- src/components/signup/OnBoardingPage.tsx
+++ src/components/signup/OnBoardingPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useContext } from "react";
+import { useRef, useContext, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import { createUser } from "../../api/methods/users";
 import { User } from "../../api/models";
@@ -7,30 +7,41 @@ import { AuthContext } from "../../context/AuthContext";
 function OnBoardingPage() {
   const nameRef = useRef<HTMLInputElement>();
 
-  const { user } = useContext(AuthContext);
+  const [hasEmptyField, setHasEmptyField] = useState(false);
+  const { user, setUser } = useContext(AuthContext);
 
-  let hasEmptyField = true;
   const onSubmit = () => {
     const name = nameRef.current?.value?.trim();
-    const user_id = user?.user?.uid;
+    const firebaseUser = user?.user;
+    const user_id = firebaseUser?.uid;
 
     if (!name || !user_id) {
-      hasEmptyField = true;
+      setHasEmptyField(true);
       return;
     }
-    hasEmptyField = false;
+    if (!firebaseUser) {
+      console.log("There is no user defined");
+      return;
+    }
+
+    setHasEmptyField(false);
     const userValues: User = {
       user_id,
       name,
     };
+
     createUser(userValues);
+    setUser({ user: firebaseUser, name });
   };
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Form.Input label="Name" input={{ ref: nameRef }} />
-      <Button type="submit">Submit</Button>
-    </Form>
+    <div>
+      {hasEmptyField ? <h3>Field cannot be blank</h3> : null}
+      <Form onSubmit={onSubmit}>
+        <Form.Input label="Name" input={{ ref: nameRef }} />
+        <Button type="submit">Submit</Button>
+      </Form>
+    </div>
   );
 }
 
